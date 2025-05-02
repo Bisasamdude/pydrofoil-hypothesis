c6464726a0ecedcc0c54f409716c35671f64f5f7
Bisasamdude <92439217+Bisasamdude@users.noreply.github.com>
2025-05-02 15:29:16 +0200

    Bitvektor anpassen an Vorgabe

--- pydrofoilhypothesis/pydrofoilhypothesis.py
+++ pydrofoilhypothesis/pydrofoilhypothesis.py
@@ -4,14 +4,12 @@ import _pydrofoil
 
 
 @st.composite
-def gen_bitvektorwidth1(draw):
-    a=_pydrofoil.bitvector(1,draw(st.integers(0,1)))
-    print(a)
-    return a
-
+def gen_bitvektorwidth1(draw,typ):
+    return _pydrofoil.bitvector(draw(st.integers(typ.width,1)),draw(st.integers(0, 2**typ.width)))#-1 weg da sonst 0#1 beim ersten auch noch ändern
+    
 def hypothesis_from_pydrofoil_type(typ):
     # takes a pydrofoil type and returns a hypothesis strategy
     if isinstance(typ, sailtypes.SmallFixedBitVector):
-        return gen_bitvektorwidth1()
+        return gen_bitvektorwidth1(typ)
     else:
         assert False, "not implemented yet"
